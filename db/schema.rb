fc16827cf474b7231a6f65329fc220ea61fb48e3
er-karim <errakkarim@gmail.com>
2024-05-07 17:26:43 +0100

    Add a migration for companies table to enforce validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_22_203751) do
-  create_table "companies", charset: "utf8mb4", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
-    t.string "name"
+ActiveRecord::Schema[7.0].define(version: 2024_05_07_161623) do
+  create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "name", null: false
     t.integer "employee_count"
-    t.string "industry"
+    t.string "industry", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name", "industry"], name: "index_companies_on_name_and_industry", unique: true
   end
 
-  create_table "deals", charset: "utf8mb4", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "deals", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
     t.string "status"

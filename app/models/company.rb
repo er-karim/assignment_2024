53e2ab6c1ede97a2aa39226ccbbdd241817e45f2
er-karim <errakkarim@gmail.com>
2024-05-14 15:02:46 +0100

    Add scopes to Company to filter by_name, by_industry and by_employee_count

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,16 @@ class Company < ApplicationRecord
   validates :name, presence: true
   validates :industry, presence: true
   validates :name, uniqueness: { scope: :industry, case_sensitive: false }
+
+  scope :by_name, lambda { |name|
+    where('companies.name LIKE ?', "%#{sanitize_sql_like(name)}%") if name.present?
+  }
+
+  scope :by_industry, lambda { |industry|
+    where('companies.industry LIKE ?', "%#{sanitize_sql_like(industry)}%") if industry.present?
+  }
+
+  scope :by_employee_count, lambda { |employee_count|
+    having('employee_count >= ?', employee_count) if employee_count.present?
+  }
 end
